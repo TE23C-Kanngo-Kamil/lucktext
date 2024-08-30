7dd1546b827ca855d31d6056699252a53f8cd9f1
Kamil Kanngo <kamil.kanngo@elev.ga.ntig.se>
2024-08-30 13:58:46 +0200

    Första

---
+++ LuckTest/Program.cs
@@ -0,0 +1,27 @@
+﻿string name;
+
+Console.ForegroundColor = ConsoleColor.Black;
+Console.BackgroundColor = ConsoleColor.White;
+Console.Clear();
+
+Console.WriteLine("Please type your name: ");
+name = Console.ReadLine();
+
+Console.Clear();
+
+string album;
+
+Console.WriteLine("Please type your favorite album: ");
+album = Console.ReadLine();
+
+Console.Clear();
+
+string food;
+
+Console.WriteLine("Please type your favorite food: ");
+food = Console.ReadLine();
+
+Console.Clear();
+
+Console.WriteLine(name + " went to their local vinyl store to look for " + album + ", but was unable to find it. As sad as this was, " + name + " didn't mind too much because they would be cooking up " + food + ", their favorite food, for dinner.");
+name = Console.ReadLine();
